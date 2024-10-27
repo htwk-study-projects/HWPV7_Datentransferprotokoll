1506fe95f698aeddf35d681fe8f286119e72b566
krapottke <krapottke_git@icloud.com>
2024-10-27 14:01:48 +0100

    einfügen der beiden beispiele von der aufgaben pdf

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <b15f/b15f.h>
 
+// Übertragen von abwechselnd 0 und 1
 int main(){
-    std::cout << "Hello World";
+    B15f& drv = B15F::getInstance();
+    drv.setRegister(&DDRA, 0x01); //Bit 7-1 Eingabe, Bit 0 Ausgabe
+    while(true){
+        //Ausgabe
+        std::cout << "Schreibe 0";
+        drv.setRegister(&PORTA, 0); // ungeschicktes Alternieren
+        std::cout << "Schreibe 1";
+        drv.setRegister(&PORTA, 1);
+    }
+
     return 0;
 }
+
+/* Asynchrones Lesen von PortA anderes PC mit seinem Board
+int main(){
+    B15f& drv = B15F::getInstance();
+    drv.setRegister(&DDRA, 0x00); //Bit 7-1 Eingabe, Bit 0 Ausgabe
+    while(true){
+    //Lesen
+        std::cout << ((int) drv.getRegister(&PINA) << endl; 
+        drv.delay_ms(10);
+    }
+
+    return 0;
+}*/

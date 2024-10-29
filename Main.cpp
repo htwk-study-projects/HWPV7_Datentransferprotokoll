8cfcb97ef146c09c58db0ba6086f2a03caf1e1a8
krapottke <krapottke_git@icloud.com>
2024-10-29 15:29:36 +0100

    1 und 0 schreiben

--- Main.cpp
+++ Main.cpp
@@ -5,17 +5,19 @@
 int main(){
     B15f& drv = B15F::getInstance();
     drv.setRegister(&DDRA, 0x01); //Bit 7-1 Eingabe, Bit 0 Ausgabe
+    int counter = 0;
     while(true){
        schreibe(drv);
        lese(drv);
+       i++;
     }
 
     return 0;
 }
 
-void schreibe(B15F & drv){
+void schreibe(B15F & drv, int i){
     std::cout << "Schreibe 0";
-    drv.setRegister(&PORTA, 0);
+    drv.setRegister(&PORTA, (i % 2));
 }
 
 void lese(B15F & drv){

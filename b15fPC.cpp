d929c1131f97d0785ddd7a1764f766e4aa301ffe
krapottke <krapottke_git@icloud.com>
2024-12-22 18:31:17 +0100

    flag angabe am anfang ob sender oder receiver

--- b15fPC.cpp
+++ b15fPC.cpp
@@ -7,18 +7,27 @@
 #include "DataBlock.hpp"
 #include "CRC.hpp"
 #include "Sender.hpp"
+#include "Receiver.hpp"
 
 //IDEE: Über ARGS* in main bestimmen ob eine Sender Instanz oder eine Empfänger Instanz erstellt wird
-
-int main() {
+// sollte umgesetzt sein
+int main(int argc, char* argv[]) {
+    if(argc < 2){
+        std::cerr << "Fehler: Bitte '-s' -> sender oder '-r' -> receiver als Argument übergeben.\n";
+        return 1;
+    }
     B15F& drv = B15F::getInstance();
     drv.setRegister(&DDRA, 0x0f);
     //drv.setRegister(&PORTA, 0x0f);
-    
-    Sender sender = Sender(drv);
+    if(strcmp(argv[1], "-s") == 0){
+        Sender sender = Sender(drv);
+        sender.createDataBlocks();
+        sender.send();
+    }
+    else if(strcmp(argv[1], "-r") == 0){
+        Reciever reciever = Reciever();
+    }
 
-    sender.createDataBlocks();
-    sender.send();
 
     drv.setRegister(&PORTA, 0);
     return 0;
